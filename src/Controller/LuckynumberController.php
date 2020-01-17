10f020953f7d69dc2ad019288f786f1c3afcade1
Jurre Boellen <jurreboellen@gmail.com>
2020-01-17 20:40:49 +0100

    Test van template

--- src/Controller/LuckynumberController.php
+++ src/Controller/LuckynumberController.php
@@ -1,14 +1,15 @@
 <?php
-
+// src/Controller/LuckyController.php
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class TestController
+
+class LuckynumberController
 {
      /**
-     * @Route("/lucky/number")
+     * @Route("/lucky/getal")
       */
     public function number()
     {
